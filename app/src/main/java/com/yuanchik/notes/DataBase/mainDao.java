6005418af4597907feaf7c4339653c1b8f2c48c5
Yuanchik <zlovcistomvide@gmail.com>
2025-12-08 14:16:10 +0800

    new database mainDao

---
+++ app/src/main/java/com/yuanchik/notes/DataBase/mainDao.java
@@ -0,0 +1,24 @@
+package com.yuanchik.notes.DataBase;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.yuanchik.notes.Moduls.Notes;
+
+import java.util.List;
+
+@Dao
+public interface mainDao {
+
+    @Insert(onConflict = REPLACE)
+    void insert(Notes notes);
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    List<Notes> getAll();
+
+    @Query("UPDATE notes SET title = :title, notes = :notes WHERE ID = :id")
+    void update(int id, String title, String notes);
+}
